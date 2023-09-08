b519ea48f3af4f34d6792058b4944f8cf65697bd
gustavojms <gustavo.joset0@hotmail.com>
2023-09-08 18:55:03 -0300

    fix: corrigindo caminho de importação do modulo de usuario

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
   imports: [UsersModule],
