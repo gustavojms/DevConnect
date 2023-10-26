5c6515acf36f5723282afccd9253e065c6f9066d
Nasc1mento <anr@discente.ifpe.edu.br>
2023-10-25 21:59:02 -0300

    adicionar varios no time

--- app/(project)/equipe/page.tsx
+++ app/(project)/equipe/page.tsx
@@ -2,7 +2,6 @@
 
 import {
   getUser,
-  submitRole,
   submitTeam,
   submitTeamMember,
 } from '@/app/services/ApiService';
@@ -20,6 +19,8 @@ import {
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
 import { useEffect, useState } from 'react';
+import Select from 'react-select';
+import { usersOptions } from './util';
 
 interface ModalProps {
   isvisible: boolean;
@@ -28,35 +29,18 @@ interface ModalProps {
 
 export default function Team({ isvisible, onClose }: ModalProps) {
   const form = useForm();
-  const roleOptions = ['desenvolvedor', 'testador', 'DBA'];
   const [users, setUsers] = useState([]);
-  const [selectedUser, setSelectedUser] = useState('');
-  const [selectedRole, setSelectedRole] = useState('');
-  // const [userCount, setUserCount] = useState(1);
-  // const [usersToSend, setUsersToSend] = useState([]);
+  const [selectedUserOption, setSelectedUserOption] = useState([]);
 
-  const handleSelectChange = (e: any) => {
-    const selectedUserId = e.target.value || users[0].userId;
-    setSelectedUser(selectedUserId);
-    const selectedValue = e.target.value;
-    setSelectedRole(selectedValue);
+  const userOptionListener = (selectedOption: any) => {
+    setSelectedUserOption(selectedOption);
+    console.log(selectedOption);
   };
-
-  // const handleAddUser = () => {
-  //   setUserCount(userCount + 1);
-  //   const user = {
-  //     memberId: parseInt(selectedUser, 10),
-  //     roleName: selectedRole || roleOptions[0],
-  //   }
-
-  //   setUsersToSend([...usersToSend, user]);
-  //   console.log(usersToSend)
-  // };
 
   useEffect(() => {
     async function fetchUsers() {
       const response = await getUser();
-      setUsers(response);
+      setUsers(usersOptions(response));
       console.log(response);
     }
 
@@ -72,21 +56,15 @@ export default function Team({ isvisible, onClose }: ModalProps) {
     });
 
     if (response != null) {
-      const userMemberId = parseInt(selectedUser, 10);
-      const dataMember = {
-        memberId: userMemberId,
-        teamId: response.teamId,
-      };
-
-      const dataMemberRole = {
-        roleName: selectedRole || roleOptions[0],
-        userId: 1,
-      };
-
-      console.log(dataMemberRole);
-
-      await submitRole(dataMemberRole);
-      await submitTeamMember(dataMember.teamId, dataMember);
+      // eslint-disable-next-line no-restricted-syntax
+      for (const user of selectedUserOption) {
+        const dataMember = {
+          memberId: user.value,
+          teamId: response.teamId,
+        };
+        // eslint-disable-next-line no-await-in-loop
+        await submitTeamMember(dataMember.teamId, dataMember);
+      }
     }
     return response;
   }
@@ -140,59 +118,18 @@ export default function Team({ isvisible, onClose }: ModalProps) {
                     </FormItem>
                   )}
                 />
-                <div className="flex flex-col text-gray-250">
-                  {/* <Button
-                  type="button"
-                  className="mx-auto mr-4 
-                bg-blue-violet-500 hover:bg-blue-600 text-white 
-                py-2 px-4 rounded-md transition duration-300 ease-in-out"
-                  onClick={handleAddUser}
-                >
-                  +
-                </Button> */}
+                <FormLabel className="text-gray-250 text-opacity-70">
+                  Membros
+                </FormLabel>
 
-                  <p>Usuário</p>
-
-                  {Array.from({ length: userCount }).map((_, index) => (
-                    // eslint-disable-next-line react/no-array-index-key
-                    <div key={index} className="flex flex-row">
-                      <select
-                        className="w-56 p-1 h-10 mt-2 mb-4 rounded-sm"
-                        id={`selectUser${index}`}
-                        value={users[index] ? users[index].username : ''}
-                        onChange={(e) => {
-                          const updatedUsers = [...users];
-                          updatedUsers[index] = {
-                            ...updatedUsers[index],
-                            username: e.target.value,
-                          };
-                          setUsers(updatedUsers);
-                        }}
-                      >
-                        {users.map((user: any) => (
-                          <option key={user.userId} value={user.username}>
-                            {user.username}
-                          </option>
-                        ))}
-                      </select>
-                      <select
-                        className="w-56 p-1 mt-2 mb-4 ml-5 rounded-sm"
-                        id={`selectRole${index}`}
-                        value={selectedRole}
-                        onChange={handleSelectChange}
-                      >
-                        <option value="desenvolvedor">Desenvolvedor</option>
-                        <option value="testador">Testador</option>
-                        <option value="DBA">DBA</option>
-                      </select>
-                    </div>
-                  ))}
-                </div>
-
-                {/* <div className="flex flex-col text-gray-250">
-                Funções
-                
-              </div> */}
+                <Select
+                  options={users}
+                  isMulti
+                  className="basic-multi-select mb-2"
+                  classNamePrefix="select"
+                  value={selectedUserOption}
+                  onChange={userOptionListener}
+                />
 
                 <Button
                   type="submit"
