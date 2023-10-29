98f354d55e3214878ab1e66561a220ce66aba6f2
andreymafra55 <andreymafra55@gmail.com>
2023-10-29 17:03:15 -0300

    Adicionando link de ver projetos

--- app/components/DropDown.tsx
+++ app/components/DropDown.tsx
@@ -7,6 +7,8 @@ import { IoMdArrowDropdown } from 'react-icons/io';
 import { MdAddCircleOutline } from 'react-icons/md';
 import { HiOutlineEye } from 'react-icons/hi';
 import { type } from 'os';
+import Link from 'next/link';
+import Listagem from '../(project)/projeto/listagem/page';
 
 type DropdownProps = {
   campo1: string;
@@ -54,10 +56,13 @@ export default function Dropdown({ campo1, campo2 }: DropdownProps) {
                 </span>
               </li>
               <li>
-                <span className="flex py-1 bg-inherit text-pale-blue font-semibold cursor-pointer">
+                <Link
+                  href="/projeto/listagem"
+                  className="flex py-1 bg-inherit text-pale-blue font-semibold cursor-pointer"
+                >
                   {campo2}
                   <HiOutlineEye className="ml-auto h-6 w-6" />
-                </span>
+                </Link>
               </li>
             </ul>
             <Modal isvisible={showModal} onClose={handleCloseModal} />
